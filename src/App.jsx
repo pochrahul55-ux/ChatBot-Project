2c050993e478e5a0b76a4973bbe6f61d725d4955
Rahul Pochaboina <Rahul.Pochaboina.Contractor@pepsico.com>
2026-05-15 12:23:32 -0400

    feat: update index.html and App component, add dayjs and babel-plugin-react-compiler dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,48 @@ import ChatInput from './components/ChatInput';
 import ChatMessages from './components/ChatMessages';
 import { useEffect } from 'react';
 import { Chatbot } from 'supersimpledev';
+import RobotProfileimage from './assets/robot.png';
 
-function App() { 
-    const [isLoading, setIsLoading] = useState(false);
-    const [chatMessages, setChatMessages] = useState(
-      JSON.parse(localStorage.getItem('messages')) || []
-    ); 
+function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [chatMessages, setChatMessages] = useState(
+    JSON.parse(localStorage.getItem('messages')) || []
+  );
 
-    useEffect(() => {
-      localStorage.setItem('messages', JSON.stringify(chatMessages));
-    }, [chatMessages]);
+  useEffect(() => {
+    localStorage.setItem('messages', JSON.stringify(chatMessages));
+  }, [chatMessages]);
 
-    useEffect (() => {
-      Chatbot.addResponses({
-        'hi': 'Hey there! How can i help you?',
-        'how is the weather outside?': 'Its sunny with 78F',
-        'whats your name?': 'I am ChatBot!'
-      })
-    }, []);  
+  useEffect(() => {
+    Chatbot.addResponses({
+      'hi': 'Hey there! How can i help you?',
+      'how is the weather outside?': 'Its sunny with 78F',
+      'whats your name?': 'I am ChatBot!'
+    })
+  }, []);
+
+  const title = `${chatMessages.length} Messages`;
+
   return (
-    <div className="chat-container">
-      <ChatMessages 
-        chatMessages={chatMessages}
-        isLoading={isLoading}
-        setIsLoading={setIsLoading}
-      />
-      <ChatInput 
-      chatMessages={chatMessages} 
-      setChatMessages={setChatMessages}
-      isLoading={isLoading}
-      setIsLoading={setIsLoading}
-      />
-    </div>
-  )     
+    <>
+      <title>{title}</title>
+      <link rel="icon" type="image/svg+xml" href={RobotProfileimage} />
+
+      <div className="chat-container">
+        <ChatMessages
+          chatMessages={chatMessages}
+          isLoading={isLoading}
+          setIsLoading={setIsLoading}
+        />
+        <ChatInput
+          chatMessages={chatMessages}
+          setChatMessages={setChatMessages}
+          isLoading={isLoading}
+          setIsLoading={setIsLoading}
+        />
+      </div>
+    </>
+  )
 }
 
 export default App;
